7f01fa3edba21ea0eeb2cfbec4526706cb25cc26
jay bodra <jaybodra01@gmail.com>
2025-01-07 03:41:24 +0530

    frontend part ui completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import './index.css'
+import "./index.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeListing from "./pages/RecipeListing";
+import RecipeView from "./pages/RecipeView";
+import RecipeForm from "./pages/RecipeForm";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-       <h1 className="text-3xl font-bold underline bg-pink-600">
-          Hello world!
-        </h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/list" element={<RecipeListing />} />
+        <Route path="/view" element={<RecipeView />} />
+        <Route path="/form" element={<RecipeForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
